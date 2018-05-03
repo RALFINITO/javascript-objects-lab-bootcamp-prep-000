d8d75499f78d62ce8a4bb2d9197300ed228b3b29
rafaelalfinito@hotmail.com <rafaelalfinito@hotmail.com>
2018-05-03 21:53:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {placeholder:"a"}
+
+function updateObjectWithKeyAndValue(obj, key, val){
+  var newObj = Object.assign(obj, {[key]: val})
+  return newObj
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, val){
+  return obj[key] = val
+}
+
